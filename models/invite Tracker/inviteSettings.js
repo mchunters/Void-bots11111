8c8822252c02402e351684c1ddaee28aaa0db246
Skmaitap <135059693+mchunters@users.noreply.github.com>
2026-09-13 18:43:27 +0530

    Update inviteSettings.js

--- models/invite Tracker/inviteSettings.js
+++ models/invite Tracker/inviteSettings.js
@@ -1,9 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const inviteSettingsSchema = new mongoose.Schema({
-    guildId: { type: String, required: true, unique: true },
-    inviteLogChannelId: { type: String, default: null },
-    status: { type: Boolean, default: true }
-});
+const inviteSettingsSchema = new mongoose.Schema(
+    {
+        guildId: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true
+        },
+        inviteLogChannelId: {
+            type: String,
+            default: null
+        },
+        status: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
 
-module.exports = mongoose.model('InviteSettings', inviteSettingsSchema);
+module.exports = mongoose.model("InviteSettings", inviteSettingsSchema);
